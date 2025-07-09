91d4bc9719f9cf3ef74396192cdb2b3ca5ae386b
David Henao <daves.hb@hotmail.com>
2025-07-09 07:30:43 -0500

    add login page

--- script.js
+++ script.js
@@ -3,9 +3,29 @@ const routes = {
   "/users": "./views/users.html",
   "/newuser": "./views/newuser.html",
   "/about": "./views/about.html",
+  "/login": "./views/login.html",
 };
 
 let counter = 0;
+
+function isAuth() {
+  const result = localStorage.getItem("Auth") || null;
+  return result;
+}
+
+async function navigate(pathname) {
+  if (!isAuth()) {
+    pathname = "/login";
+  }
+
+  const route = routes[pathname] || routes["/"];
+  const html = await fetch(route).then((res) => res.text());
+  document.getElementById("content").innerHTML = html;
+  history.pushState({}, "", pathname);
+
+  if (pathname === "/about") setupCounter();
+  if (pathname === "/login") setupLoginForm();
+}
 
 document.body.addEventListener("click", (e) => {
   if (e.target.matches("[data-link]")) {
@@ -15,21 +35,17 @@ document.body.addEventListener("click", (e) => {
   }
 });
 
-async function navigate(pathname) {
-  const route = routes[pathname];
-  const html = await fetch(route).then((res) => res.text());
-  document.getElementById("content").innerHTML = html;
-  history.pushState({}, "", pathname);
-
+function setupCounter() {
   const counterValue = document.getElementById("counter-value");
   const incrementBtn = document.getElementById("increment-btn");
   const decrementBtn = document.getElementById("decrement-btn");
 
-  incrementBtn.addEventListener("click", () => {
+  incrementBtn?.addEventListener("click", () => {
     counter++;
     counterValue.textContent = counter;
   });
-  decrementBtn.addEventListener("click", () => {
+
+  decrementBtn?.addEventListener("click", () => {
     counter--;
     counterValue.textContent = counter;
   });
@@ -38,5 +54,37 @@ async function navigate(pathname) {
 window.addEventListener("popstate", () => {
   console.log("se hizo clic");
   console.log(location);
+  navigate(location.pathname);
+});
+
+function setupLoginForm() {
+  const userAuth = "admin";
+  const passAuth = "1234";
+
+  const form = document.getElementById("login-spa");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("password").value;
+
+    if (user === userAuth && pass === passAuth) {
+      localStorage.setItem("Auth", "true");
+      navigate("/users");
+    } else {
+      alert("usuario o contraseña son incorrectos");
+    }
+  });
+}
+
+const buttonCloseSession = document.getElementById("close-sesion");
+buttonCloseSession.addEventListener("click", () => {
+  localStorage.setItem("Auth", "false");
+  navigate("/login");
+});
+
+
+window.addEventListener("DOMContentLoaded", () => {
   navigate(location.pathname);
 });
